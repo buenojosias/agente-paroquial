2c2930b622c7f6e0b8750da79c1c10e1535470bd
Josias Bueno <messias_jpb@hotmail.com>
2026-05-25 22:49:35 -0300

    Adjusted DatabaseSeeder to add first parish and admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Parish;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -10,12 +11,31 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        Parish::create([
+            'name' => 'Paróquia São Marcos',
+            'address' => 'Rua Roberto Gava, 310 - Pilarzinho',
+            'city' => 'Curitiba',
+            'state' => 'PR',
+            'is_active' => true,
+        ]);
+
+        User::factory()->create([
+            'name' => 'Administrador',
+            'email' => 'admin@email.com',
+            'password' => bcrypt('123456'),
+            'is_active' => true,
+            'is_admin' => true,
+        ]);        
+
         User::factory()->create([
             'name' => 'Josias Bueno',
             'email' => 'josias@email.com',
             'password' => bcrypt('123456'),
-            'roles' => ['admin'],
             'is_active' => true,
+            'is_admin' => false,
+            'selected_parish_id' => 1,
+            'selected_parish_name' => 'Paróquia São Marcos',
+            'created_by' => 1,
         ]);
     }
 }
